b8b91d10037b0535e8e99de59964354fb4359c35
Mikhail Morozov <plzombie@yandex.ru>
2020-04-04 13:49:36 +0300

    Add threads.h stub

---
+++ threads_h/BUILD_QMAKE/types_and_defs/types_and_defs.pro
@@ -0,0 +1,4 @@
+
+INCLUDEPATH = ../../include
+
+SOURCES = ../../tests/types_and_defs.c
---
+++ threads_h/include/threads.h
@@ -0,0 +1,111 @@
+/*
+BSD 2-Clause License
+
+Copyright (c) 2020, Mikhail Morozov
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef THREADS_H
+#define THREADS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <time.h>
+
+//#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+#define thread_local _Thread_local
+//#else
+//#define thread_local
+//#endif
+
+#define ONCE_FLAG_INIT 0
+
+//TODO: Set Next
+
+//#define TSS_DTOR_ITERATIONS
+
+typedef void *cnd_t;
+typedef void *thrd_t;
+typedef void *tss_t;
+typedef void *mtx_t;
+typedef void (* tss_dtor_t)(void *);
+typedef int (* thrd_start_t)(void *);
+typedef unsigned int once_flag;
+
+enum {
+	mtx_plain,
+	mtx_recursive,
+	mtx_timed
+};
+
+enum {
+	thrd_timedout,
+	thrd_success,
+	thrd_busy,
+	thrd_error,
+	thrd_nomem
+};
+
+void call_once(once_flag *flag, void (* func)(void));
+
+int cnd_broadcast(cnd_t *cond);
+int cnd_destroy(cnd_t *cond);
+int cnd_init(cnd_t *cond);
+int cnd_signal(cnd_t *cond);
+int cnd_timedwait(cnd_t *restrict cond, mtx_t *restrict mtx, const struct timespec *restrict ts);
+int cnd_wait(cnd_t *cond, mtx_t *mtx);
+
+void mtx_destroy(mtx_t *mtx);
+int mtx_init(mtx_t *mtx, int type);
+int mtx_lock(mtx_t *mtx);
+int mtx_timedlock(mtx_t *restrict mtx, const struct timespec *restrict ts);
+int mtx_trylock(mtx_t *mtx);
+int mtx_unlock(mtx_t *mtx);
+
+int thrd_create(thrd_t *thr, thrd_start_t func, void *arg);
+thrd_t thrd_current(void);
+int thrd_detach(thrd_t thr);
+int thrd_equal(thrd_t thr0, thrd_t thr1);
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+_Noreturn void thrd_exit(int res);
+#else
+void thrd_exit(int res);
+#endif
+void thrd_exit(int res);
+int thrd_join(thrd_t thr, int *res);
+int thrd_sleep(const struct timespec *duration, struct timespec *remaining);
+int thrd_yield(void);
+
+int tss_create(tss_t *key, tss_dtor_t dtor);
+void tss_delete(tss_t key);
+void *tss_get(tss_t key);
+int tss_set(tss_t key, void *val);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
